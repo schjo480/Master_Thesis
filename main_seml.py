144d8df70ed19cc76f5a88633380678c29f05298
Joe Schmit <ge34xoc@mytum.de>
2024-08-26 07:24:29 +0200

    Sample valid trajectories

--- main_seml.py
+++ main_seml.py
@@ -52,7 +52,7 @@ def main(data, wandb, diffusion_config, model, training, testing, eval):
             features += feature + '_'
         sample_binary_list, sample_list, ground_truth_hist, ground_truth_fut, ground_truth_fut_binary = model.get_samples(load_model=False, task='predict')
         torch.save(sample_list, '/ceph/hdd/students/schmitj/MA_Diffusion_based_trajectory_prediction/experiments/' + f'{wandb['exp_name']}' + '/' + f'{model_config['transition_mat_type']}' + '_' + f'{diffusion_config['type']}' + '/samples_raw_' + features + f'hist{data['history_len']}_fut_{data['future_len']}.pth')
-        fut_ratio, f1, acc, tpr, avg_sample_length, sample_list, ground_truth_hist, ground_truth_fut = model.eval(sample_binary_list, sample_list, ground_truth_hist, ground_truth_fut, ground_truth_fut_binary, return_samples=True)
+        fut_ratio, f1, acc, tpr, avg_sample_length, sample_list, valid_ids, ground_truth_hist, ground_truth_fut = model.eval(sample_binary_list, sample_list, ground_truth_hist, ground_truth_fut, ground_truth_fut_binary, return_samples=True)
         torch.save(sample_list, '/ceph/hdd/students/schmitj/MA_Diffusion_based_trajectory_prediction/experiments/' + f'{wandb['exp_name']}' + '/' + f'{model_config['transition_mat_type']}' + '_' + f'{diffusion_config['type']}' + '/samples_' + features + f'hist{data['history_len']}_fut_{data['future_len']}.pth')
         torch.save(ground_truth_hist, '/ceph/hdd/students/schmitj/MA_Diffusion_based_trajectory_prediction/experiments/' + f'{wandb['exp_name']}' + '/' + f'{model_config['transition_mat_type']}' + '_' + f'{diffusion_config['type']}' + '/gt_hist_' + features + f'hist{data['history_len']}_fut_{data['future_len']}.pth')
         torch.save(ground_truth_fut, '/ceph/hdd/students/schmitj/MA_Diffusion_based_trajectory_prediction/experiments/' + f'{wandb['exp_name']}' + '/' + f'{model_config['transition_mat_type']}' + '_' + f'{diffusion_config['type']}' + '/gt_fut_' + features + f'hist{data['history_len']}_fut_{data['future_len']}.pth')
@@ -69,6 +69,9 @@ def main(data, wandb, diffusion_config, model, training, testing, eval):
         print("\n")
         print("Val TPR", tpr)
         #wandb.log({"Val TPR, mult samples": tpr})
+        print("\n")
+        valid_ids = [item for sublist in valid_ids for item in sublist]
+        print("Avg. number of samples until valid", sum(valid_ids) / len(valid_ids))
         print("\n")
         print("Average sample length", avg_sample_length)
         print("\n")
